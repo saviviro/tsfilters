e32cdc5441b0a940ef9502d7c50ae88b3e0aacc3
saviviro <savi1993@gmail.com>
2021-02-24 10:26:51 +0200

    updated the data to the end of 2020

--- R/data.R
+++ R/data.R
@@ -1,9 +1,9 @@
 #' U.S. industrial production index: INDPRO
 #'
 #' A dataset containing monthly, seasonally adjusted U.S. industrial production
-#' index (2012=100) from 1919 January to 2020 September.
+#' index (2012=100) from 1919 January to 2020 December.
 #'
-#' @format A univariate class 'ts' time series object containing 1221 observations.
+#' @format A univariate class 'ts' time series object containing 1224 observations.
 #'
 #' @source \url{https://fred.stlouisfed.org/series/INDPRO}
 "INDPRO"
